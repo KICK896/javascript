4905c1b426e2c8243d4bffdde56b6a256538ba32
KICK896 <it1936@cemk.ac.in>
2025-02-14 05:07:21 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 123
+let accountName = "Ritesh"
+var accountPassword = "12345"
+accountCity = "Tamluk"
+let accountState; // undefined
+
+// accountId = 1234
+// console.log(accountId) -- Not Possible... iT's constant value .. It can't be change
+
+// accountName = "Rishab"
+// console.log(accountName)
+
+console.table([accountId,accountName,accountPassword,accountCity,accountState])
